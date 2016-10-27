8fb4bf842954157de8455cccd64a75a8f0fcf0c2
adityachauhan18 <aditya.c@techtreeit.com>
2016-10-27 18:06:08 +0530

    add

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,6 +1,12 @@
 $('#quantity').click(function(){
     
   var quantity=  $('#quantity').val();
+  
+  var prize = $('product-inner-price').find('ins').val();
+  
+  alert(prize);
+  
+  
   console.log($('.product-inner').val());
   
 
